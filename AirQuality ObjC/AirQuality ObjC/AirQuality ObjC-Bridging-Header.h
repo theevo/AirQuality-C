97788bb5e8a55738afa4b651600162a8bd1bc9e8
RYAN GREENBURG <ryangreenburg@RYANs-MacBook-Pro-2.local>
2019-12-03 08:24:41 -0700

    Add starter branch

--- AirQuality ObjC/AirQuality ObjC/AirQuality ObjC-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "DVMCityAirQualityController.h"
-#import "DVMCityAirQuality.h"
-#import "DVMWeather.h"
-#import "DVMPollution.h"
