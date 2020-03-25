813b0217c0072fba948444cf1a04dc8a28dad88b
Theo Vora <theo.vora@gmail.com>
2020-03-25 17:13:50 -0600

    draft view controller. not working yet.

---
+++ AirQuality ObjC/AirQuality ObjC/Resources/BridgingHeader.h
@@ -0,0 +1,12 @@
+//
+//  AirQuality ObjC-Bridging-Header.h
+//  AirQuality ObjC
+//
+//  Created by theevo on 3/25/20.
+//  Copyright © 2020 Theo Vora. All rights reserved.
+//
+
+#import "TTVCityAirQualityController.h"
+#import "TTVCityAirQuality.h"
+#import "TTVWeather.h"
+#import "TTVPollution.h"
